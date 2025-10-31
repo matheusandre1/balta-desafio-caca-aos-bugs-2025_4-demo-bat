7cb3623b8a646d50b21c085aaaffeedcc2eae481
Matheus Andre <matheusandr2@gmail.com>
2025-10-31 19:02:08 -0300

    feat: update projets and adding unit test basic

---
+++ src/BugStore.Domain.Tests/Entities/CustomerTest.cs
@@ -0,0 +1,63 @@
+﻿using BugStore.Domain.Entities;
+
+namespace BugStore.Domain.Tests.Entities;
+public class CustomerTest
+{
+    private static Customer ValidCustomer()
+    {
+        return new Customer
+        {
+            Id = Guid.NewGuid(),
+            Name = "Alice Pais Maravilhas",
+            Phone = "11999989712",
+            BirthDate = new DateTime(1999, 12, 31),
+        };
+    }
+
+    [Fact]
+
+    public void TheCustomerFieldCannotBeEmpty() 
+    {
+        var customer = ValidCustomer();        
+
+        Assert.NotNull(customer);
+    }
+
+    [Fact]
+
+    public void TheCustomerFieldIDCannotBeEmpty()
+    {
+        var customer = ValidCustomer().Id;       
+
+        Assert.NotEqual(Guid.Empty,customer);
+    }
+
+    [Fact]
+
+    public void TheCustomerFieldNameCannotBeEmpty()
+    {
+        var customer = ValidCustomer().Name;
+
+        
+
+        Assert.NotEqual("", customer);
+    }
+
+    [Fact]
+    public void TheCustomerFieldEmailCannotBeEmpty()
+    {
+        var customer = ValidCustomer().Email;
+
+        Assert.NotEqual("", customer);
+    }
+
+    [Fact]
+    public void TheCustomerFieldBirthDateCannotBeEmpty()
+    {
+        var customer = ValidCustomer().BirthDate = DateTime.Now;
+
+        var email = DateTime.Now;
+
+        Assert.NotEqual(email, customer);
+    }
+}
--- src/BugStore.Domain/Entities/Customer.cs
+++ src/BugStore.Domain/Entities/Customer.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BugStore.Domain.Entities;
 
 public class Customer
 {
-    public Guid Id { get; set; }
+    [Key]
+    public Guid Id { get;  set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
